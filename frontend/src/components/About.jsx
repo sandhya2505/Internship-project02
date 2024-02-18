548f8c292cff988904ff77acfad5c8109067844e
sandhya2505 <sandhyanayak2505@gmail.com>
2024-02-18 17:14:05 +0530

    Initial commit

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { HiOutlineArrowRight } from "react-icons/hi";
+
+const About = () => {
+  return (
+    <>
+      <section className="about" id="about">
+        <div className="container">
+          <div className="banner">
+            <div className="top">
+              <h1 className="heading">ABOUT US</h1>
+              <p>The only thing we're serious about is food.</p>
+            </div>
+            <p className="mid">
+              Our kitchen is the heart of "Food Junction", where skilled chefs
+              craft each dish with love and attention to detail. We source the
+              finest, freshest ingredients to create a menu that celebrates both
+              classic and innovative flavors. From traditional favorites to
+              contemporary creations, our diverse menu reflects a commitment to
+              quality and culinary excellence.
+            </p>
+            <Link to={"/"}>
+              Explore Menu{" "}
+              <span>
+                <HiOutlineArrowRight />
+              </span>
+            </Link>
+          </div>
+          <div className="banner">
+            <img src="about.png" alt="about" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default About;
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">Food Junction</div>
+          <div className="right">
+            <p>NMAMIT-Nitte, Karkala</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By Sandhya Nayak</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By Sandhya Nayak.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
